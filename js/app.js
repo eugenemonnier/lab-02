a924f62e15f61fb2940087558de5ccae5f6b21a7
EugeneMonnier <eugene.monnier@gmail.com>
2020-01-08 10:40:46 -0800

    finish display images feature

--- js/app.js
+++ js/app.js
@@ -4,14 +4,30 @@ let allHorns = [];
 function Animal(animalObj) {
   this.imageUrl = animalObj.image_url;
   this.title = animalObj.title;
+  this.description = animalObj.description;
   this.keyword = animalObj.keyword;
   this.horns = animalObj.horns;
   allHorns.push(this);
 }
 
+Animal.prototype.render = function() {
+  const myTemplate = $('#photo-template').html();
+  const $newSection = $('<section></section>');
+
+  $newSection.html(myTemplate);
+  $newSection.find('h2').text(this.title);
+  $newSection.find('img').attr('src', this.imageUrl);
+  $newSection.find('img').attr('alt', this.title);
+  $newSection.find('p').text(this.description);
+
+  $('main').append($newSection);
+
+};
+
 $.ajax('data/page-1.json', {method: 'GET', dataType: 'JSON'})
   .then (data => {
     data.forEach(element => {
       let horny = new Animal(element);
+      horny.render();
     });
   });
